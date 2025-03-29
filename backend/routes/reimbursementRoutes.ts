42e846e97159d855b35462e61997379e9a75aac0
Sheraz Waseem <26100026@lums.edu.pk>
2025-03-29 17:03:33 +0500

    All models APIs made (api.ts left)

--- backend/routes/reimbursementRoutes.ts
+++ backend/routes/reimbursementRoutes.ts
@@ -1,8 +1,18 @@
 import express from "express";
-import { getAllReimbursements } from "../controllers/reimursementController";
+import {
+    getAllReimbursements,
+    createReimbursement,
+    getReimbursementsByTripId,
+    getReimbursementsByAdminId,
+    getReimbursementsByStatus
+} from "../controllers/reimursementController";
 
 const router = express.Router();
 
 router.get("/", getAllReimbursements);
+router.post("/", createReimbursement);
+router.get("/trip/:trip_id", getReimbursementsByTripId);
+router.get("/admin/:admin_id", getReimbursementsByAdminId);
+router.get("/status/:status", getReimbursementsByStatus);
 
 export default router;
