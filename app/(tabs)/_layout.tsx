80703231cbdd98a03b85d59a0c51df09b85e2ce9
haseeb-ashfaq <haseeb_2003@outlook.com>
2025-03-30 16:05:03 +0500

    final

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -82,6 +82,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="loginForm"
+        options={{
+          title: 'Login Form',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
