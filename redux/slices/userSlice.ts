3273a1c1eee5595ac6dba590cd79c0f96621c798
Sheraz Waseem <26100026@lums.edu.pk>
2025-04-02 21:20:12 +0500

    Completed Redux

--- redux/slices/userSlice.ts
+++ redux/slices/userSlice.ts
@@ -1,14 +1,17 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
+  id: number;  
   name: string;
   email: string;
-    
+  isAdmin: boolean;
 }
 
 const initialState: UserState = {
+  id: 0,
   name: "",
   email: "",
+  isAdmin: false,
 };
 
 const userSlice = createSlice({
@@ -16,12 +19,16 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action: PayloadAction<UserState>) => {
+      state.id = action.payload.id;
       state.name = action.payload.name;
       state.email = action.payload.email;
+      state.isAdmin = action.payload.isAdmin;
     },
     resetUser: (state) => {
+      state.id = 0; 
       state.name = "";
       state.email = "";
+      state.isAdmin = false;
     },
   },
 });
