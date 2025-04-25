f7d839b369fcf2c84795907077347f85b3db6ac2
hania2370 <haniahash2370@outlook.com>
2025-04-25 18:19:51 +0500

    Removed remaining comments from backend

--- backend/db.ts
+++ backend/db.ts
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-// Load environment variables
 dotenv.config();
-
-// Define a function to connect to MongoDB
 const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
