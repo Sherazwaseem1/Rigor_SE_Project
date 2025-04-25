04d102bac97922ff82a8904c17da7ab15965faa2
haseeb-ashfaq <haseeb_2003@outlook.com>
2025-04-25 23:53:59 +0500

    rem consl

--- backend/db.ts
+++ backend/db.ts
@@ -5,9 +5,7 @@ dotenv.config();
 const connectDB = async (): Promise<void> => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string);
-        console.log("✅ MongoDB Connected!");
     } catch (error) {
-        console.error("❌ MongoDB Connection Error:", error);
         process.exit(1);
     }
 };
